97157ed86fb9da75a9b8588e1422c58d88cb9450
Luciano Jr <luciano@lucianojr.com.br>
2017-05-21 13:04:53 +0100

    Add ide_helper and make migrations

--- config/app.php
+++ config/app.php
@@ -177,7 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        \Rebing\GraphQL\GraphQLServiceProvider::class,
+        Rebing\GraphQL\GraphQLServiceProvider::class,
 
     ],
 
---
+++ database/migrations/2017_05_21_120057_create_user_profiles_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_profiles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('avatar');
+            $table->integer('user_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('user_id')
+                ->references('id')
+                ->on('users')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_profiles');
+    }
+}
